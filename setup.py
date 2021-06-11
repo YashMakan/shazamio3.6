a59e68d84f4af95270ae622b32cc82f3a4567305
Yash <yashmakan261@gmail.com>
2021-06-12 01:01:25 +0530

    initial

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='shazamyash',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='An example python package',
+    long_description=open('README.txt').read(),
+    install_requires=['aiofiles','aiohttp','async-timeout==3.0.1','attrs==20.3.0','chardet==3.0.4','dataclass-factory==2.10.1','idna==3.1','multidict==5.1.0','numpy','pydub==0.24.1','typing-extensions==3.7.4.3','yarl==1.6.3'],
+    author='Bill Mills',
+    author_email='myemail@example.com'
+)
